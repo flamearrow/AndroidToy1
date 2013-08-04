a8a9e6dcde0dee9352d4ad5b324d4864f94597fc
flamearrow <arrow.cen@gmail.com>
2013-08-04 18:00:34 -0400

    Adding mlgb

--- CCWeibo/src/com/example/ccweibo/FavouriteTwitterAct.java
+++ CCWeibo/src/com/example/ccweibo/FavouriteTwitterAct.java
@@ -31,7 +31,7 @@ public class FavouriteTwitterAct extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_favtwitter);
-
+		// MLGB!
 		// should have a file names 'searches'
 		savedSearches = getSharedPreferences("searches", MODE_PRIVATE);
 		// all ui components are inherited from View
